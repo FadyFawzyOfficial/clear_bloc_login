a995fe542a884ed7f6fbfa7f5aaae5360815b531
Fady Fawzy <FadyFawzy.Official@gmail.com>
2022-10-11 13:47:03 +0200

    Build Login Bloc Provider that will Provide the Login Bloc

---
+++ lib/login/bloc/login_provider.dart
@@ -0,0 +1,14 @@
+import 'login_bloc.dart';
+import 'package:flutter/material.dart';
+
+class Provider extends InheritedWidget {
+  final bloc = Bloc();
+
+  Provider({Key? key, required Widget child}) : super(key: key, child: child);
+
+  @override
+  bool updateShouldNotify(oldWidget) => true;
+
+  static Bloc of(BuildContext context) =>
+      (context.getElementForInheritedWidgetOfExactType() as Provider).bloc;
+}
